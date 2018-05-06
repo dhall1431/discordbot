22fb00dde190977865c187c6f3d3896ce857f2aa
dhall1431 <36953473+dhall1431@users.noreply.github.com>
2018-05-06 01:40:05 -0700

    Delete Ref.java

--- src/main/java/com/github/dhall1431/jda/Ref.java
+++
@@ -1,6 +0,0 @@
-package com.github.dhall1431.jda;
-
-public class Ref {
-	public static final String TOKEN ="NDQyMDUwMDg3NDM5NDMzNzUw.Dc5U4w.4VAee7XPt_Z0zBeODoF5ODYjOa0";
-	public static final String CLIEN_ID = "442050087439433750";
-}
